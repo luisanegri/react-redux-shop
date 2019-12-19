5f2ab8c67e9a6c0b3236eee4156e00103df1230c
luisanegri <luisaanegri@gmail.com>
2019-12-19 18:36:53 +0100

    Updates

---
+++ precache-manifest.f65cea80befaf7998ae85d25c9e79840.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ecd54287197b86b6e9e3087e4b2e61c",
+    "url": "/react-redux-shop/index.html"
+  },
+  {
+    "revision": "6751a5ceaf45e4026ce0",
+    "url": "/react-redux-shop/static/css/2.c36072e5.chunk.css"
+  },
+  {
+    "revision": "4b5688d7f4ffd9803ead",
+    "url": "/react-redux-shop/static/css/main.a3cb2b12.chunk.css"
+  },
+  {
+    "revision": "6751a5ceaf45e4026ce0",
+    "url": "/react-redux-shop/static/js/2.527c48b3.chunk.js"
+  },
+  {
+    "revision": "4b5688d7f4ffd9803ead",
+    "url": "/react-redux-shop/static/js/main.7e81c484.chunk.js"
+  },
+  {
+    "revision": "bef74363cf6a69128196",
+    "url": "/react-redux-shop/static/js/runtime-main.8af70863.js"
+  },
+  {
+    "revision": "123b7d37cd8d85017b593ad45c328a13",
+    "url": "/react-redux-shop/static/media/close.123b7d37.svg"
+  }
+]);
