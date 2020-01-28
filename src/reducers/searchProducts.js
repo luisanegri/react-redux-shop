c58bf4744555733e63f0b51cfecb670ec85fa2f5
luisanegri <luisaanegri@gmail.com>
2020-01-28 21:23:44 +0100

    remove query unused

--- src/reducers/searchProducts.js
+++ src/reducers/searchProducts.js
@@ -1,18 +1,14 @@
-import { SEARCH } from "../actions/searchProducts";
+import { SEARCH } from '../actions/searchProducts';
 
 const reducer = (state = [], action = {}) => {
-  console.log("action test:", action);
   switch (action.type) {
     case SEARCH:
-      const { products, query } = action.payload;
-      // const products = action.payload.products
-      // const query = action.payload.query
-
+      const { products } = action.payload;
       const filtered = products.filter(product => {
         return product.name.includes(action.payload.query);
       });
 
-      console.log("filtered test:", filtered);
+      console.log('filtered test:', filtered);
 
       return filtered;
     default:
