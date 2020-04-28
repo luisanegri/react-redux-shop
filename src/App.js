218c5d103605fae8266f400cb9a5ffb5f5d57222
luisanegri <luisaanegri@gmail.com>
2020-04-28 15:45:50 +0200

    updating app to redirect to home when user is signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import ProductsContainer from './components/ProductsContainer';
 import ProductDetailContainer from './components/ProductDetailContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -65,7 +65,16 @@ class App extends React.Component {
           <Route path="/product/:id" component={ProductDetailContainer}></Route>
           <Route path="/cart" component={CartContainer} />
           <Route path="/wishlist" component={WishListContainer}></Route>
-          <Route path="/signin" component={SignInAndSignUpPage} />
+          <Route
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            }
+          />
         </Switch>
         <Footer />
       </div>
@@ -73,8 +82,12 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
