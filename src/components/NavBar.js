1c8277f0501a2df4b573924ab88c408ba86d472d
luisanegri <luisaanegri@gmail.com>
2020-05-10 17:46:58 +0200

    fix nagivation button toggle bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,72 +1,93 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { auth } from '../firebase/firebase.utils';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default function NavBar({ cart, currentUser, itemCount }) {
   return (
-    <div className="nav-c">
-      <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand nav-brand-custom" to="/">
-          <span>Shop </span>
-          <span>
-            <i className="fa fa-circle"></i>
-          </span>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarTogglerDemo02"
-          aria-controls="navbarTogglerDemo02"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0 navbar-nav-custom">
-            <li className="nav-item active">
-              <Link className="nav-link link-custom" to="/products">
-                Products
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand href="/" className="nav-brand-custom">
+        <span>Shop </span>
+        <span>
+          <i className="fa fa-circle"></i>
+        </span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto navbar-nav-custom">
+          <div className="nav-item">
+            <Link className="nav-link link-custom" to="/products">
+              Products
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link link-custom" to="/cart">
+              {itemCount}
+              <i className="fa fa-shopping-cart"></i>
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link className="nav-link link-custom" to="/wishlist">
+              Wishlist
+              <i className="fa fa-heart"></i>
+            </Link>
+          </div>
+          {currentUser ? (
+            <div className="nav-item">
+              <div
+                className="nav-link link-custom"
+                onClick={() => auth.signOut()}
+              >
+                Sign out
+              </div>
+            </div>
+          ) : (
+            <div className="nav-item">
+              <Link
+                to="/signin"
+                className="nav-link link-custom"
+                onClick={() => auth.signOut()}
+              >
+                Sign in
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link link-custom" to="/cart">
-                {itemCount}
+            </div>
+          )}
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
 
-                <i className="fa fa-shopping-cart"></i>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link link-custom" to="/wishlist">
-                Wishlist
-                <i className="fa fa-heart"></i>
-              </Link>
-            </li>
-            {currentUser ? (
-              <li className="nav-item">
-                <div
-                  className="nav-link link-custom"
-                  onClick={() => auth.signOut()}
-                >
-                  Sign out
-                </div>
-              </li>
-            ) : (
-              <li className="nav-item">
-                <Link
-                  to="/signin"
-                  className="nav-link link-custom"
-                  onClick={() => auth.signOut()}
-                >
-                  Sign in
-                </Link>
-              </li>
-            )}
-            <li className="nav-item"></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+    // <div className="nav-c">
+    //   <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+    //     <Link className="navbar-brand nav-brand-custom" to="/">
+
+    //     </Link>
+    //     <button
+    //       className="navbar-toggler"
+    //       type="button"
+    //       data-toggle="collapse"
+    //       data-target="#navbarTogglerDemo02"
+    //       aria-controls="navbarTogglerDemo02"
+    //       aria-expanded="false"
+    //       aria-label="Toggle navigation"
+    //     >
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+    //     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+    //       <ul className="navbar-nav mr-auto mt-2 mt-lg-0 navbar-nav-custom">
+    //         <li className="nav-item active">
+
+    //         </li>
+    //         <li className="nav-item">
+
+    //         </li>
+    //         <li className="nav-item">
+
+    //         </li>
+
+    //         <li className="nav-item"></li>
+    //       </ul>
+    //     </div>
+    //   </nav>
+    // </div>
   );
 }
